3285bbccd86d6b66e6f83f8cac837f51d1d5c4e9
shahareldad <shahareldad@gmx.com>
2015-12-16 23:06:48 +0300

    Clean code

--- src/com/eldad/memorygame/mainFragment.java
+++ src/com/eldad/memorygame/mainFragment.java
@@ -13,7 +13,6 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MainFragment extends Fragment implements IHaveSetup {
 	
